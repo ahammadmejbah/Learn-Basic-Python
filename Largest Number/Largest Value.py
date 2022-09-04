feb82bf58b2a03d52f03e4fff553281f9595737d
MEJBAH AHAMMAD <56669333+ahammadmejbah@users.noreply.github.com>
2022-09-04 12:39:39 +0600

    Initial Commit

---
+++ Largest Number/Largest Value.py
@@ -0,0 +1,15 @@
+    """ Deep Neuron AI """
+    
+def largest(num1,num2,num3):
+    result = num1
+    if(num2 > result):
+        result = num2
+    if(num3 > result):
+        result = num3
+    print("Largest : ",result
+
+x=int(input("Enter 1st number : "))
+y=int(input("Enter 2nd number : "))
+z=int(input("Enter 3rd number : "))
+
+largest(x,y,z)
