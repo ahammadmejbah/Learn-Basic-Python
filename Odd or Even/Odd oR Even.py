5363bcf689dab6d0ed7b477196dcd17350a92813
MEJBAH AHAMMAD <56669333+ahammadmejbah@users.noreply.github.com>
2022-09-04 12:34:35 +0600

    Initial Commit

---
+++ Odd or Even/Odd oR Even.py
@@ -0,0 +1,11 @@
+""" Deep neuron AI"""
+
+def Odd_oR_Even(number):
+    if(number % 2 == 0):
+        print(number, " is even")
+    else:
+        print(number, " is odd")
+
+x=int(input("Enter number : "))
+
+Odd_oR_Even(x)
